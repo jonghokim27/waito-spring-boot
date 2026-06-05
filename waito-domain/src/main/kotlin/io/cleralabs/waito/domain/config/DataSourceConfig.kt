1f01a5646ac03a7dbc265a8fffd3d87567c0bb1f
jonghokim27 <joey0307@naver.com>
2026-06-06 04:25:20 +0900

    LazyConnectionDataSourceProxy 적용

---
+++ waito-domain/src/main/kotlin/io/cleralabs/waito/domain/config/DataSourceConfig.kt
@@ -0,0 +1,24 @@
+package io.cleralabs.waito.domain.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.jdbc.autoconfigure.DataSourceProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy
+import javax.sql.DataSource
+
+@Configuration(proxyBeanMethods = false)
+class DataSourceConfig {
+    @Bean
+    @ConfigurationProperties("spring.datasource.hikari")
+    fun actualDataSource(properties: DataSourceProperties): DataSource {
+        return properties.initializeDataSourceBuilder().build()
+    }
+
+    @Bean
+    @Primary
+    fun dataSource(actualDataSource: DataSource): DataSource {
+        return LazyConnectionDataSourceProxy(actualDataSource)
+    }
+}
