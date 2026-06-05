668ab997660a56808b32cf49cd370bc60e61aa2a
jonghokim27 <joey0307@naver.com>
2026-06-06 02:57:44 +0900

    캐시 적용 및 메트릭 수집 (오류 수정)

--- waito-common/waito-adapter/src/main/kotlin/io/cleralabs/waito/adapter/redis/RedisCacheAdapter.kt
+++ waito-common/waito-adapter/src/main/kotlin/io/cleralabs/waito/adapter/redis/RedisCacheAdapter.kt
@@ -1,19 +1,20 @@
 package io.cleralabs.waito.adapter.redis
 
 import io.cleralabs.waito.core.port.CachePort
-import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Component
+import java.time.Duration
 
 @Component
 class RedisCacheAdapter(
-    private val redisTemplate: RedisTemplate<String, String>
+    private val redisTemplate: StringRedisTemplate,
 ) : CachePort {
     override fun get(key: String): String? {
         return redisTemplate.opsForValue().get(key)
     }
 
     override fun set(key: String, value: String, ttl: Long) {
-        redisTemplate.opsForValue().set(key, value, ttl)
+        redisTemplate.opsForValue().set(key, value, Duration.ofSeconds(ttl))
     }
 
 }
